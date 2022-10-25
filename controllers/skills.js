d95c61a2d372c44fdb9d1d1303fe7b8ed45907ec
madeiramcqueen <madeira@mcqueen.net>
2022-10-25 16:54:27 -0700

    Complete lab

---
+++ controllers/skills.js
@@ -0,0 +1,19 @@
+// routes/skills.js
+const Skill = require('../models/skill')
+
+
+module.exports = {
+    index
+}
+
+function index(req, res) {
+    res.render('skills/index', {
+        skills: Skill.getAll()
+    });
+}
+
+// function show(req, res) {
+//     res.render('skills/show', {
+//         skill: Skill.getOne(req.params.id),
+//     });
+// }
---
+++ models/skill.js
@@ -0,0 +1,10 @@
+// list of skills
+const skills = ['JavaScript', 'HTML', 'CSS', 'Express']
+
+function getAll() {
+    return skills
+}
+
+module.exports = {
+    getAll
+}
