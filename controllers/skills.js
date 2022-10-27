05844c2f051a14b0a102806002db937f20d9123d
madeiramcqueen <madeira@mcqueen.net>
2022-10-26 18:03:47 -0700

    Add new skill page

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,13 +3,23 @@ const Skill = require('../models/skill')
 
 
 module.exports = {
-    index
+    index,
+    create,
+    newSkill
 }
 
 function index(req, res) {
     res.render('skills/index', {
         skills: Skill.getAll()
     });
+}
+
+function create(req, res) {
+    res.send('skills/create') 
+}
+
+function newSkill(req, res) {
+    res.send('skills/new')
 }
 
 // function show(req, res) {
--- routes/skills.js
+++ routes/skills.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var skillsCtrl = require('../controllers/skills');
 
+// Route paths
 router.get('/', skillsCtrl.index);
+router.post('/', skillsCtrl.create);
+router.get('/new', skillsCtrl.newSkill);
 
 module.exports = router;
